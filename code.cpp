7d0163fdb66c7565e493c317ed814878fcbc0e2b
Ilia Shishkov <shishkov.ilia@gmail.com>
2023-07-21 18:30:29 +0300

    Fix Max in second_attempt

--- code.cpp
+++ code.cpp
@@ -3,6 +3,7 @@
 #include <format>
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <optional>
 #include <sstream>
 #include <unordered_map>
@@ -125,7 +126,7 @@ auto operator+(const Expression<T>& lhs, const Expression<U>& rhs) {
 }
 
 // clang-format off
-template <typename T, typename U> class MaxExpr : public Expression<Sum<T, U>> {
+template <typename T, typename U> class MaxExpr : public Expression<MaxExpr<T, U>> {
   // clang-format on
 private:
   const T& lhs;
@@ -164,6 +165,10 @@ Estimated CalcCompleteAt(const EstimationData& eta) {
   return V(eta.delivery_started_at) + V(eta.delivery_duration);
 }
 
+Estimated CalcCompleteAtWithMax(const EstimationData& eta) {
+  return V(eta.delivery_started_at) + Max(V(eta.delivery_duration), V(0s));
+}
+
 bool SomeCondition();
 
 // Estimated DoesntCompile(const EstimationData& eta) {
@@ -184,6 +189,158 @@ bool SomeCondition() { return true; }
 
 }  // namespace second_attempt
 
+// namespace third_attempt {
+
+// // clang-format off
+// template <typename T> class Expression {
+// // clang-format on
+// public:
+//   virtual ~Expression() = default;
+
+//   virtual T Evaluate() const = 0;
+
+// private:
+//   virtual void FillVariablesTo(
+//       std::unordered_map<std::string, std::string>& variables) const = 0;
+//   virtual void FillFormulaTo(std::ostream& output) const = 0;
+
+// public:
+//   std::unordered_map<std::string, std::string> GetVariables() const {
+//     std::unordered_map<std::string, std::string> variables;
+//     FillVariablesTo(variables);
+//     return variables;
+//   }
+
+//   virtual std::string GetFormula() const {
+//     std::ostringstream output;
+//     FillFormulaTo(output);
+//     return output.str();
+//   }
+// };
+
+// // clang-format off
+// template <typename T> struct Value : Expression<T> {
+// // clang-format on
+//   const T& value;
+//   std::string name;
+
+//   Value(const T& value, std::string_view name) : value(value), name(name) {}
+
+//   T Evaluate() const override { return value; }
+
+//   void FillVariablesTo(
+//       std::unordered_map<std::string, std::string>& variables) const override {
+//     variables[name] = ToString(value);
+//   }
+
+//   void FillFormulaTo(std::ostream& output) const override { output << name; }
+// };
+
+// // clang-format off
+// template <typename T> auto MakeValue(const T& x, std::string_view name) {
+// // clang-format on
+//   return std::make_shared<Value<T>>(x, name);
+// }
+
+// template <typename T, typename U>
+// using SumType_t = decltype(std::declval<T>() + std::declval<U>());
+
+// template <typename T, typename U>
+// class Sum : public Expression<SumType_t<T, U>> {
+// private:
+//   shared_ptr<Expression<T>> lhs_;
+//   shared_ptr<Expression<U>> rhs_;
+
+// public:
+//   Sum(shared_ptr<Expression<T>> lhs, shared_ptr<Expression<U>> rhs)
+//       : lhs_(std::move(lhs)), rhs_(std::move(rhs)) {}
+
+//   SumType_t<T, U> Evaluate() const override {
+//     return lhs_->Evaluate() + rhs_->Evaluate();
+//   }
+
+//   void FillVariablesTo(
+//       std::unordered_map<std::string, std::string>& variables) const override {
+//     lhs_->FillVariablesTo(variables);
+//     rhs_->FillVariablesTo(variables);
+//   }
+
+//   void FillFormulaTo(std::ostream& output) const override {
+//     lhs_->FillFormulaTo(output);
+//     output << " + ";
+//     rhs_->FillFormulaTo(output);
+//   }
+// };
+
+// template <typename U, template <class> typename T, typename V,
+//           template <class> typename W>
+// std::enable_if_t<std::is_base_of_v<Expression<U>, T<U>> &&
+//                      std::is_base_of_v<Expression<V>, W<V>>,
+//                  shared_ptr<Sum<U, V>>>
+// operator+(shared_ptr<T<U>> lhs, shared_ptr<W<V>> rhs) {
+//   return std::make_shared<Sum<U, V>>(std::move(lhs), std::move(rhs));
+// }
+
+// // clang-format off
+// template <typename T, typename U> class MaxExpr : public Expression<Sum<T, U>> {
+//   // clang-format on
+// private:
+//   const T& lhs;
+//   const U& rhs;
+
+// public:
+//   MaxExpr(const Expression<T>& lhs, const Expression<U>& rhs)
+//       : lhs(lhs.AsChild()), rhs(rhs.AsChild()) {}
+
+//   auto Evaluate() const { return std::max(lhs.Evaluate(), rhs.Evaluate()); }
+
+//   void FillVariablesTo(
+//       std::unordered_map<std::string, std::string>& variables) const {
+//     lhs.FillVariablesTo(variables);
+//     rhs.FillVariablesTo(variables);
+//   }
+
+//   void FillFormulaTo(std::ostream& output) const {
+//     output << "max(";
+//     lhs.FillFormulaTo(output);
+//     output << ", ";
+//     rhs.FillFormulaTo(output);
+//     output << ')';
+//   }
+// };
+
+// template <typename T, typename U>
+// auto Max(const Expression<T>& lhs, const Expression<U>& rhs) {
+//   return MaxExpr(lhs, rhs);
+// }
+
+// #define V(x) \
+//   Value { (x), #x }
+
+// Estimated CalcCompleteAt(const EstimationData& eta) {
+//   return V(eta.delivery_started_at) + V(eta.delivery_duration);
+// }
+
+// bool SomeCondition();
+
+// // Estimated DoesntCompile(const EstimationData& eta) {
+// //   unknown delivery_duration_expr;
+// //   if (SomeCondition()) {
+// //     delivery_duration_expr = V(eta.fallback_delivery_duration);
+// //   } else {
+// //     delivery_duration_expr =
+// //         V(eta.arrival_to_customer_at) - V(eta.delivery_started_at);
+// //   }
+
+// //   return Max(delivery_duration_expr, V(0s));
+// // }
+
+// bool SomeCondition() { return true; }
+
+// #undef V
+
+// }  // namespace third_attempt
+
 int main(int arc, char* argv[]) {
   const EstimationData eta{
       .delivery_started_at = chrono::system_clock::now() - chrono::hours(2),
@@ -196,6 +353,10 @@ int main(int arc, char* argv[]) {
   }
   {
     auto estimated = second_attempt::CalcCompleteAt(eta);
+    cout << estimated.formula << " = " << ToString(estimated.value) << endl;
+  }
+  {
+    auto estimated = second_attempt::CalcCompleteAtWithMax(eta);
     cout << estimated.formula << " = " << ToString(estimated.value) << endl;
   }
 }
