af2e90030cee379f2aaa61ce5066d0d07aec08e5
Ilia <shishkov.ilia@gmail.com>
2023-07-21 18:45:23 +0300

    Implemented third attempt

--- code.cpp
+++ code.cpp
@@ -189,97 +189,95 @@ bool SomeCondition() { return true; }
 
 }  // namespace second_attempt
 
-// namespace third_attempt {
+namespace third_attempt {
 
-// // clang-format off
-// template <typename T> class Expression {
-// // clang-format on
-// public:
-//   virtual ~Expression() = default;
+// clang-format off
+template <typename T> class Expression {
+// clang-format on
+public:
+  virtual ~Expression() = default;
 
-//   virtual T Evaluate() const = 0;
+  virtual T Evaluate() const = 0;
 
-// private:
-//   virtual void FillVariablesTo(
-//       std::unordered_map<std::string, std::string>& variables) const = 0;
-//   virtual void FillFormulaTo(std::ostream& output) const = 0;
+  virtual void FillVariablesTo(
+      std::unordered_map<std::string, std::string>& variables) const = 0;
+  virtual void FillFormulaTo(std::ostream& output) const = 0;
 
-// public:
-//   std::unordered_map<std::string, std::string> GetVariables() const {
-//     std::unordered_map<std::string, std::string> variables;
-//     FillVariablesTo(variables);
-//     return variables;
-//   }
+  std::unordered_map<std::string, std::string> GetVariables() const {
+    std::unordered_map<std::string, std::string> variables;
+    FillVariablesTo(variables);
+    return variables;
+  }
 
-//   virtual std::string GetFormula() const {
-//     std::ostringstream output;
-//     FillFormulaTo(output);
-//     return output.str();
-//   }
-// };
+  virtual std::string GetFormula() const {
+    std::ostringstream output;
+    FillFormulaTo(output);
+    return output.str();
+  }
+};
 
-// // clang-format off
-// template <typename T> struct Value : Expression<T> {
-// // clang-format on
-//   const T& value;
-//   std::string name;
+// clang-format off
+template <typename T> struct Value : Expression<T> {
+// clang-format on
+  const T& value;
+  std::string name;
 
-//   Value(const T& value, std::string_view name) : value(value), name(name) {}
+  Value(const T& value, std::string_view name) : value(value), name(name) {}
 
-//   T Evaluate() const override { return value; }
+  T Evaluate() const override { return value; }
 
-//   void FillVariablesTo(
-//       std::unordered_map<std::string, std::string>& variables) const override {
-//     variables[name] = ToString(value);
-//   }
+  void FillVariablesTo(
+      std::unordered_map<std::string, std::string>& variables) const override {
+    variables[name] = ToString(value);
+  }
 
-//   void FillFormulaTo(std::ostream& output) const override { output << name; }
-// };
+  void FillFormulaTo(std::ostream& output) const override { output << name; }
+};
 
-// // clang-format off
-// template <typename T> auto MakeValue(const T& x, std::string_view name) {
-// // clang-format on
-//   return std::make_shared<Value<T>>(x, name);
-// }
+// clang-format off
+template <typename T> auto MakeValue(const T& x, std::string_view name) {
+// clang-format on
+  return std::make_shared<Value<T>>(x, name);
+}
 
-// template <typename T, typename U>
-// using SumType_t = decltype(std::declval<T>() + std::declval<U>());
+template <typename T, typename U>
+using SumType_t = decltype(std::declval<T>() + std::declval<U>());
 
-// template <typename T, typename U>
-// class Sum : public Expression<SumType_t<T, U>> {
-// private:
-//   shared_ptr<Expression<T>> lhs_;
-//   shared_ptr<Expression<U>> rhs_;
+template <typename T, typename U>
+class Sum : public Expression<SumType_t<T, U>> {
+private:
+  shared_ptr<Expression<T>> lhs_;
+  shared_ptr<Expression<U>> rhs_;
 
-// public:
-//   Sum(shared_ptr<Expression<T>> lhs, shared_ptr<Expression<U>> rhs)
-//       : lhs_(std::move(lhs)), rhs_(std::move(rhs)) {}
+public:
+  Sum(shared_ptr<Expression<T>> lhs, shared_ptr<Expression<U>> rhs)
+      : lhs_(std::move(lhs)), rhs_(std::move(rhs)) {}
 
-//   SumType_t<T, U> Evaluate() const override {
-//     return lhs_->Evaluate() + rhs_->Evaluate();
-//   }
+  SumType_t<T, U> Evaluate() const override {
+    return lhs_->Evaluate() + rhs_->Evaluate();
+  }
 
-//   void FillVariablesTo(
-//       std::unordered_map<std::string, std::string>& variables) const override {
-//     lhs_->FillVariablesTo(variables);
-//     rhs_->FillVariablesTo(variables);
-//   }
+  void FillVariablesTo(
+      std::unordered_map<std::string, std::string>& variables) const override {
+    lhs_->FillVariablesTo(variables);
+    rhs_->FillVariablesTo(variables);
+  }
 
-//   void FillFormulaTo(std::ostream& output) const override {
-//     lhs_->FillFormulaTo(output);
-//     output << " + ";
-//     rhs_->FillFormulaTo(output);
-//   }
-// };
+  void FillFormulaTo(std::ostream& output) const override {
+    lhs_->FillFormulaTo(output);
+    output << " + ";
+    rhs_->FillFormulaTo(output);
+  }
+};
 
-// template <typename U, template <class> typename T, typename V,
-//           template <class> typename W>
-// std::enable_if_t<std::is_base_of_v<Expression<U>, T<U>> &&
-//                      std::is_base_of_v<Expression<V>, W<V>>,
-//                  shared_ptr<Sum<U, V>>>
-// operator+(shared_ptr<T<U>> lhs, shared_ptr<W<V>> rhs) {
-//   return std::make_shared<Sum<U, V>>(std::move(lhs), std::move(rhs));
-// }
+template <typename U, template <class> typename T, typename V,
+          template <class> typename W>
+std::enable_if_t<std::is_base_of_v<Expression<U>, T<U>> &&
+                     std::is_base_of_v<Expression<V>, W<V>>,
+                 shared_ptr<Sum<U, V>>>
+operator+(shared_ptr<T<U>> lhs, shared_ptr<W<V>> rhs) {
+  return std::make_shared<Sum<U, V>>(std::move(lhs), std::move(rhs));
+}
 
 // // clang-format off
 // template <typename T, typename U> class MaxExpr : public Expression<Sum<T, U>> {
@@ -314,32 +312,39 @@ bool SomeCondition() { return true; }
 //   return MaxExpr(lhs, rhs);
 // }
 
-// #define V(x) \
-//   Value { (x), #x }
+template <typename T>
+Estimated BuildEstimated(std::shared_ptr<Expression<T>> expr) {
+  return Estimated{.value = expr->Evaluate(),
+                     .formula = expr->GetFormula(),
+                     .variables = expr->GetVariables()};
+}
 
-// Estimated CalcCompleteAt(const EstimationData& eta) {
-//   return V(eta.delivery_started_at) + V(eta.delivery_duration);
+#define V(x) \
+  MakeValue (x, #x)
+
+Estimated CalcCompleteAt(const EstimationData& eta) {
+  return BuildEstimated(V(eta.delivery_started_at) + V(eta.delivery_duration));
+}
+
+bool SomeCondition();
+
+// Estimated DoesntCompile(const EstimationData& eta) {
+//   unknown delivery_duration_expr;
+//   if (SomeCondition()) {
+//     delivery_duration_expr = V(eta.fallback_delivery_duration);
+//   } else {
+//     delivery_duration_expr =
+//         V(eta.arrival_to_customer_at) - V(eta.delivery_started_at);
+//   }
+
+//   return Max(delivery_duration_expr, V(0s));
 // }
 
-// bool SomeCondition();
+bool SomeCondition() { return true; }
 
-// // Estimated DoesntCompile(const EstimationData& eta) {
-// //   unknown delivery_duration_expr;
-// //   if (SomeCondition()) {
-// //     delivery_duration_expr = V(eta.fallback_delivery_duration);
-// //   } else {
-// //     delivery_duration_expr =
-// //         V(eta.arrival_to_customer_at) - V(eta.delivery_started_at);
-// //   }
+#undef V
 
-// //   return Max(delivery_duration_expr, V(0s));
-// // }
-
-// bool SomeCondition() { return true; }
-
-// #undef V
-
-// }  // namespace third_attempt
+}  // namespace third_attempt
 
 int main(int arc, char* argv[]) {
   const EstimationData eta{
